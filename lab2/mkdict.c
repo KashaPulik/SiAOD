72eb8eeadc988c7ed261684619254a3e2c8ae1ca
Kulik Pavel <KashaPulik@users.noreply.github.com>
2023-03-24 11:16:25 +0700

    Create mkdict app

--- lab2/mkdict.c
+++ lab2/mkdict.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <math.h>
+#include <stdint.h>
 
-void mkdict(char* filename)
+void mkdict(char* filename, uint32_t nwords, uint32_t wordlen)
 {
 	FILE* file = fopen(filename, "w");
-	if(file == NULL)
+	if(file == NULL) {
+		printf("Can't make file\n");
 		exit(1);
-	for(int i = 0; i < 400000; i++) {
-		for(int j = 0; j < 8; j++) {
+	}
+	for(int i = 0; i < nwords; i++) {
+		for(int j = 0; j < wordlen; j++) {
 			fprintf(file, "%c", rand() % 26 + 97);
 		}
 		fprintf(file, "\n");
@@ -16,8 +19,13 @@ void mkdict(char* filename)
 	fclose(file);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	mkdict("slova.txt");
+	if(argc != 4) {
+		printf("Wrong number of args\n");
+		exit(1);
+	}
+	mkdict(argv[1], (uint32_t) atoi(argv[2]), (uint32_t) atoi(argv[3]));
+	printf("Succesfull!\n");
 	return 0;
 }
