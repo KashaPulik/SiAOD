a3a42ac47c36cf95c4df52c3a528705ef85d5522
Kulik Pavel <pashakulik58@gmail.com>
2023-10-27 01:14:08 +0700

    ABOBA

--- 3sem/2lab/src/main.c
+++ 3sem/2lab/src/main.c
@@ -3,12 +3,16 @@
 int main()
 {
     trie* tree = trie_create();
-    tree = trie_insert(tree, "ABOBA", "aboba");
-    tree = trie_insert(tree, "ABOB", "aboba");
-    tree = trie_insert(tree, "ABOGA", "abob");
+    tree = trie_insert(tree, "ABOBA", "a");
+    tree = trie_insert(tree, "ABOBB", "b");
+    tree = trie_insert(tree, "ABOBC", "c");
+    tree = trie_insert(tree, "ABOBDA", "d");
+    tree = trie_insert(tree, "ABOBDB", "d");
+    tree = trie_insert(tree, "ABOBDBA", "d");
+    tree = trie_insert(tree, "BABUSHKA", "e");
+    tree = trie_insert(tree, "BABA", "e");
     trie_display(tree);
     tree = trie_delete(tree, "ABOBA");
-    tree = trie_delete(tree, "ABOB");
-    tree = trie_delete(tree, "ABOGA");
-    free(tree);
+    tree = trie_delete(tree, "ABOBB");
+    trie_display(tree);
 }
