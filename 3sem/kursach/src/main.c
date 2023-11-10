c7ba769bdc3b58ffe00eae4524bb58513f538c90
Kulik Pavel <pashakulik58@gmail.com>
2023-11-10 11:24:41 +0700

    Deletion don't works right so i am starting refactoring with tst_delete

--- 3sem/kursach/src/main.c
+++ 3sem/kursach/src/main.c
@@ -1,42 +1,31 @@
 #include "tst.h"
 #include <stdio.h>
 
-int n_words = 14;
+int n_words = 1000;
 
 int main()
 {
     tst* tree = NULL;
-    char** strings = malloc(sizeof(char*) * n_words);
-    for (int i = 0; i < n_words; i++) {
+    char** strings = malloc(sizeof(char*) * (n_words + 1));
+    FILE* dict = fopen("aboba.txt", "r");
+    for (int i = 1; i <= n_words; i++) {
         strings[i] = malloc(11);
+        fscanf(dict, "%s", strings[i]);
     }
-    strings[0] = "aboba";
-    strings[1] = "babka";
-    strings[2] = "abobab";
-    strings[3] = "pisa";
-    strings[4] = "popa";
-    strings[5] = "kisa";
-    strings[6] = "pusia";
-    strings[7] = "ya";
-    strings[8] = "lublu";
-    strings[9] = "kakat";
-    strings[10] = "i";
-    strings[11] = "pafu";
-    strings[12] = "kushat";
-    strings[13] = "mau";
-    for (int i = 0; i < n_words; i++) {
+    for (int i = 1; i <= n_words; i++) {
         tree = tst_insert(tree, strings[i]);
     }
-    tst_print_all_words(tree);
-    printf("\n");
 
-    for (int i = n_words - 1; i >= 0; i--) {
+    for (int i = n_words; i > 0; i--) {
         tree = tst_delete(tree, strings[i]);
-        printf("After deleting %s\n", strings[i]);
-        tst_print_all_words(tree);
-        printf("\n");
     }
-    tst_print_all_words(tree);
+
+    // for (int i = 1; i <= n_words; i++) {
+    //     tree = tst_delete(tree, strings[i]);
+    // }
+
     if (tree == NULL)
         printf("yey\n");
+    else
+        tst_print_all_words(tree);
 }
